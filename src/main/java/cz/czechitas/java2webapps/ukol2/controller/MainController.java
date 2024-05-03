4530cc192dc8405c210ca97d67da87cd96582da7
Olinka <olga.titzenthaler@gmail.com>
2024-05-04 01:10:17 +0200

    Tarrot reading @GetMapping("/threecards") repaird to pick unique 3 numbers,save those as the variables and use them for readTarotMeaning

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -33,25 +33,30 @@ public class MainController {
 
     @GetMapping("/threecards")
     public ModelAndView threeTarrotCards() {
-        Set<Integer> selectedCards = new HashSet<>();
-        List<String> tarotMeanings = new ArrayList<>(); // Declare tarotMeanings list outside the loop
-
-        while (selectedCards.size() < 3) {
-            int randomCardNumber = random.nextInt(0, 22);
-            selectedCards.add(randomCardNumber);
-            String tarotMeaning = readTarotMeaning(randomCardNumber);
-            tarotMeanings.add(tarotMeaning); // Add the tarotMeaning to the list
+        Set<Integer> allCardNumbers = new LinkedHashSet<>();
+        for (int i=0; i <=77; i++){
+            allCardNumbers.add(i);
         }
 
+        List<Integer> shuffledCardNumbers= new ArrayList<>(allCardNumbers);
+        Collections.shuffle(shuffledCardNumbers); //Shuffle the list
+
+        List<Integer> selectedCards = shuffledCardNumbers.subList(0, 3);
+
         int randomBackgroundPicture = random.nextInt(1, 8);
+        String tarotMeaning1 = readTarotMeaning(selectedCards.get(0));
+        String tarotMeaning2 = readTarotMeaning(selectedCards.get(1));
+        String tarotMeaning3 = readTarotMeaning(selectedCards.get(2));
 
         ModelAndView modelAndView = new ModelAndView("threecards");
 
-        modelAndView.addObject("card1", selectedCards.stream().findFirst().orElse(null));
-        modelAndView.addObject("card2", selectedCards.stream().skip(1).findFirst().orElse(null));
-        modelAndView.addObject("card3", selectedCards.stream().skip(2).findFirst().orElse(null));
+        modelAndView.addObject("card1", selectedCards.get(0));
+        modelAndView.addObject("card2", selectedCards.get(1));
+        modelAndView.addObject("card3", selectedCards.get(2));
+        modelAndView.addObject("tarotMeaning1", tarotMeaning1);
+        modelAndView.addObject("tarotMeaning2", tarotMeaning2);
+        modelAndView.addObject("tarotMeaning3", tarotMeaning3);
         modelAndView.addObject("randomBackground", randomBackgroundPicture);
-        modelAndView.addObject("tarotMeanings", tarotMeanings); // Add the tarotMeanings list
 
         return modelAndView;
     }
